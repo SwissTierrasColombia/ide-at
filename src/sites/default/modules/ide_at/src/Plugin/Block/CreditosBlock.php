cb14bd80fd3091a1b15253b5577015feec52b827
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-02-21 01:54:42 -0500

    Se realiza bloque para créditos

---
+++ src/sites/default/modules/ide_at/src/Plugin/Block/CreditosBlock.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\ide_at\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'CreditosBlock' block.
+ *
+ * @Block(
+ *  id = "creditos_block",
+ *  admin_label = @Translation("Créditos Agencia de Implementación"),
+ * )
+ */
+class CreditosBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+            'licencia' => '2018 | Diseñado y Desarrollado por Agencia de Implementación del Proyecto Modernización de la Administración de Tierras en Colombia, financiado por la Cooperación Suiza (SECO).',
+          ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['licencia'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Licencia'),
+    '#description' => $this->t('Escribe el texto de la licencia o créditos'),
+      '#default_value' => $this->configuration['licencia'],
+      '#weight' => '0',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['licencia'] = $form_state->getValue('licencia');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['creditos_block_licencia']['#markup'] = '<p>' . $this->configuration['licencia'] . '</p>';
+
+    return $build;
+  }
+
+}
