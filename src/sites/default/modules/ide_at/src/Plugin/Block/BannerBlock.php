cb23330da31de68498287305fd44acdb58598f4f
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-02-22 20:02:40 -0500

    Se modifica el banner para mostrar título del nodo

--- src/sites/default/modules/ide_at/src/Plugin/Block/BannerBlock.php
+++ src/sites/default/modules/ide_at/src/Plugin/Block/BannerBlock.php
@@ -18,8 +18,21 @@ class BannerBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    //$page_title = \Drupal::configFactory()->get('system.site')->get('name');
+    // link: https://www.drupal.org/node/2067859
+    // link: https://drupal.stackexchange.com/questions/187519/how-do-i-show-the-current-node-title-in-page-html-twig-or-in-node-html-twig/187520
+    //$config = $this->getConfiguration();
+    $request = \Drupal::request();
+    $route_match = \Drupal::routeMatch();
+    $page_title = '';
+    if ($route_match->getParameter('node')) {
+      $page_title = \Drupal::service('title_resolver')->getTitle($request, $route_match->getRouteObject());
+    }
+    //debug($config);
+    //$page_title = $this->t("'%name' block", array('%name' => $info[$block->delta]['info']));//block name
     $build = [];
     $build['banner_block']['#markup'] = 'Implement BannerBlock.';
+    $build['banner_block']['#title'] = $page_title;
 
     return $build;
   }
